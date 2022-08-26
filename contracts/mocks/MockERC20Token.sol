047936694a1112467b7bc10f266c891dedc2f79f
dan13ram <dan13ram@gmail.com>
2022-08-26 13:57:51 +0530

    removed solmate dependency

--- contracts/mocks/MockERC20Token.sol
+++ contracts/mocks/MockERC20Token.sol
@@ -6,11 +6,11 @@ pragma solidity 0.8.16;
 //   ║═╬╗│ │├┤ └─┐ │ ║  ├─┤├─┤││││└─┐
 //   ╚═╝╚└─┘└─┘└─┘ ┴ ╚═╝┴ ┴┴ ┴┴┘└┘└─┘
 
-import "solmate/src/tokens/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract MockERC20Token is ERC20 {
+contract MockERC20Token is ERC20Permit {
     // solhint-disable-next-line no-empty-blocks
-    constructor() ERC20("token", "TOKEN", 18) {}
+    constructor() ERC20Permit("token") ERC20("token", "TOKEN") {}
 
     function mint(address _to, uint256 _amount) external {
         _mint(_to, _amount);
